72614a96d924899698b35490d16b9f5301265f80
Brandon Truong <brandon.truong@outlook.com>
2019-07-07 19:42:41 +1000

    new set up

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-const Index = () => (
-    <div>
-        <p>Hello World! yay</p>
-    </div>
+import App from '../components/App'
+import Header from '../components/Header'
+import Submit from '../components/Submit'
+import PostList from '../components/PostList'
+
+export default () => (
+  <App>
+    <Header />
+    <Submit />
+    <PostList />
+  </App>
 )
-export default Index
