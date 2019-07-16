0a6781e7cd46703090a8ba76e683de591b5f5935
Brandon Truong <brandon.truong@outlook.com>
2019-07-16 21:05:19 +1000

    added grid component and global style

--- components/Header.js
+++ components/Header.js
@@ -1,42 +1,30 @@
 import Link from 'next/link';
+import { Flex, Box } from '@rebass/grid';
 
 const Header = ({ pathname }) => (
   <header>
-    <h1>GraphCMS Starter blog</h1>
-    <nav>
-      <Link prefetch href='/'>
-        <a className={pathname === '/' && 'is-active'}>
-          Home
-        </a>
-      </Link>
-      <Link prefetch href='/about'>
-        <a className={pathname === '/about' && 'is-active'}>
-          About
-        </a>
-      </Link>
-    </nav>
-    <style jsx>{`
-      header {
-        padding: 24px 32px;
-        margin-bottom: 32px;
-        text-align: center;
-      }
-      h1 {
-        font-weight: 100;
-      }
-      nav {
-        max-width: 650px;
-        margin: 0 auto;
-      }
-      a {
-        font-size: 18px;
-        margin-right: 16px;
-      }
-      .is-active {
-        font-weight: bold;
-        text-decoration: underline;
-      }
-    `}</style>
+
+    <Flex justifyContent="space-between" >
+      <Box>
+        <h1>Brandon Truong</h1>
+        <div>Work portfolio</div>
+      </Box>
+      <Box>
+
+        <nav>
+          <Link prefetch href='/'>
+            <a className={pathname === '/' && 'is-active'}>
+              Home
+            </a>
+          </Link>
+          <Link prefetch href='/about'>
+            <a className={pathname === '/about' && 'is-active'}>
+              About
+            </a>
+          </Link>
+        </nav>
+      </Box>
+    </Flex>
   </header>
 );
 
