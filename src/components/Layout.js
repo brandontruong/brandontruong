edb576d2385be0ad4921bc856a7ed59e72e59133
Brandon Truong <brandon.truong@sca.com.au>
2020-05-24 22:00:48 +1000

    added navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar'
 
 const Layout = ({ children }) => (
   <div>
     <div>Layout here</div>
+    <Navbar />
     <div>{children}</div>
   </div>
 )
